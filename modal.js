75c327357e477a44708a752c83ab9cb10a4adc0d
Godicks <matvej.novik@gmail.com>
2021-03-04 04:45:49 +0300

    made slider (add some simple interactive) + drop-down-menu

--- modal.js
+++ modal.js
@@ -77,3 +77,63 @@ modalMapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalMap.classList.remove("visible");
 });
+
+/*JS for slider */
+
+const slider = document.querySelector(".slider");
+const sliderFirst = slider.querySelector(".slider-first");
+const sliderSecond = slider.querySelector(".slider-second");
+const sliderThird = slider.querySelector(".slider-third");
+const switchFirst = document.querySelector(".switch-first");
+const switchSecond = document.querySelector(".switch-second");
+const switchThird = document.querySelector(".switch-third");
+
+switchFirst.addEventListener("click",function (evt) {
+  evt.preventDefault;
+  if (switchSecond.classList.contains("slider-open")){
+    switchSecond.classList.remove("slider-open");
+  } else if (switchThird.classList.contains("slider-open")){
+    switchThird.classList.remove("slider-open");
+  }
+  switchFirst.classList.add("slider-open");
+  if (!sliderSecond.classList.contains("hide")) {
+    sliderSecond.classList.add("hide");
+  }
+  if (!sliderThird.classList.contains("hide")) {
+    sliderThird.classList.add("hide");
+  }
+  sliderFirst.classList.remove("hide");
+});
+switchSecond.addEventListener("click",function (evt) {
+  evt.preventDefault;
+  if (switchFirst.classList.contains("slider-open")){
+    switchFirst.classList.remove("slider-open");
+  } 
+  if (switchThird.classList.contains("slider-open")){
+    switchThird.classList.remove("slider-open");
+  }
+  switchSecond.classList.add("slider-open");
+  if (!sliderFirst.classList.contains("hide")) {
+    sliderFirst.classList.add("hide");
+  }
+  if (!sliderThird.classList.contains("hide")) {
+    sliderThird.classList.add("hide");
+  }
+  sliderSecond.classList.remove("hide");
+});
+switchThird.addEventListener("click",function (evt) {
+  evt.preventDefault;
+  if (switchSecond.classList.contains("slider-open")){
+    switchSecond.classList.remove("slider-open");
+  } else if (switchFirst.classList.contains("slider-open")){
+    switchFirst.classList.remove("slider-open");
+  }
+  switchThird.classList.add("slider-open");
+  if (!sliderFirst.classList.contains("hide")) {
+    sliderFirst.classList.add("hide");
+  }
+  if (!sliderSecond.classList.contains("hide")) {
+    sliderSecond.classList.add("hide");
+  }
+  sliderThird.classList.remove("hide");  
+});
